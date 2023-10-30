c22d68cb2bdd35dba01a6d9b85856104cc07d93f
Surender <surenderoffcl2002@gmail.com>
2023-10-30 19:15:40 +0530

    pattern 17 still working 18 finished

--- patterns/pattern17.cpp
+++ patterns/pattern17.cpp
@@ -7,13 +7,20 @@ int main() {
     cin >>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
-            cout<<"  ";
+            cout<<" ";
         }
+        char ch = 'A';
         for(int j=0;j<2*i+1;j++){
-            
+            if(j<i){
+                cout << ch;
+                ch +=1;
+            }if(j>i){
+                cout << ch;
+ch--;
+            }
         }
         for(int j=0;j<n-i-1;j++){
-            cout<<"  ";
+            cout<<" ";
         }
         cout<<endl;
     }
